c26156e5f84e1a31398d035620edbf2b94b75e48
catac1 <catac1@users.noreply.github.com>
2026-06-03 11:16:37 +0900

    lift arbitrary password creation rule

--- users/password.go
+++ users/password.go
@@ -6,19 +6,19 @@ import (
 
 	"golang.org/x/crypto/bcrypt"
 
-	fberrors "github.com/filebrowser/filebrowser/v2/errors"
+	//fberrors "github.com/filebrowser/filebrowser/v2/errors"
 )
 
 // ValidateAndHashPwd validates and hashes a password.
 func ValidateAndHashPwd(password string, minimumLength uint) (string, error) {
-	if uint(len(password)) < minimumLength {
+	// NOTE: Disable password rule checking. Too much friction
+	/*if uint(len(password)) < minimumLength {
 		return "", fberrors.ErrShortPassword{MinimumLength: minimumLength}
 	}
 
 	if _, ok := commonPasswords[password]; ok {
 		return "", fberrors.ErrEasyPassword
-	}
-
+	}*/
 	return HashPwd(password)
 }
 
